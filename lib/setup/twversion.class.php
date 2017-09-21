1b77fa5624267f973c4c9f48d4e4d17b866d8f6c
jonnybradley <jonnybradley@users.sourceforge.net>
2017-09-21 20:46:08 +0000

    [REL] Closing 12.12 release

--- lib/setup/twversion.class.php
+++ lib/setup/twversion.class.php
@@ -27,7 +27,7 @@ class TWVersion
 		$this->branch 	= 'stable';
 
 		// Set everything else, including defaults.
-		$this->version 	= '12.12';	// needs to have no spaces for releases
+		$this->version 	= '12.13svn';	// needs to have no spaces for releases
 		$this->star	= 'Altair';
 		$this->releases	= array();
 
