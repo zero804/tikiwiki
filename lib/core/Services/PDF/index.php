c98078adc115288f2e432b96f77616463a0aaa9c
amnabilal <amnabilal@users.sourceforge.net>
2017-10-04 13:53:20 +0000

    [bp/r63722][Fix]Moved PDF related function storeTable out of AutoSave/Controller.php in new PDF/Controller.php

---
+++ lib/core/Services/PDF/index.php
@@ -0,0 +1,10 @@
+<?php
+// (c) Copyright 2002-2016 by authors of the Tiki Wiki CMS Groupware Project
+// 
+// All Rights Reserved. See copyright.txt for details and a complete list of authors.
+// Licensed under the GNU LESSER GENERAL PUBLIC LICENSE. See license.txt for details.
+// $Id: index.php 63733 2017-09-05 14:45:40Z drsassafras $
+
+// This redirects to the sites root to prevent directory browsing
+header("location: ../../../../index.php");
+die;
