f0d5a10700cd75f24606687d10bfb432b886fc55
robertokir <robertokir@users.sourceforge.net>
2017-12-11 18:47:21 +0000

    [bp/r64871][FIX] Page comment on copied page was misleading.

--- lib/wiki/wikilib.php
+++ lib/wiki/wikilib.php
@@ -222,6 +222,8 @@ class WikiLib extends TikiLib
 
 		$info = $tikilib->get_page_info($name);
 		$ip = $tikilib->get_ip_address();
+		$version = $info['version'];
+		$comment = tr("Initial content copied from version %0 of page %1", $version, $name);
 
 		if (!$info) {
 			return false;
@@ -236,7 +238,7 @@ class WikiLib extends TikiLib
 			0,
 			$info['data'],
 			$tikilib->now,
-			$info['comment'],
+			$comment,
 			$user,
 			$ip,
 			$info['description'],
