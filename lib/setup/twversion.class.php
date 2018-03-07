c5a09641616f6903f53a0673383fd15925dfd73a
lfagundes <lfagundes@users.sourceforge.net>
2018-03-07 14:43:45 +0000

    releasing 17.2

--- lib/setup/twversion.class.php
+++ lib/setup/twversion.class.php
@@ -27,7 +27,7 @@ class TWVersion
 		$this->branch 	= 'stable';
 
 		// Set everything else, including defaults.
-		$this->version 	= '17.2svn';	// needs to have no spaces for releases
+		$this->version 	= '17.2';	// needs to have no spaces for releases
 		$this->star	= 'Zeta Boötis';
 		$this->releases	= array();
 
