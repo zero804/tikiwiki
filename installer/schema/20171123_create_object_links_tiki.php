19dc413c29d0725b84e344b8cbe1682b1042fd88
luciash <luci@tiki.org>
2018-05-29 19:58:13 +0000

    [bp/r66546][FIX] Installer: fatal error caused by installer upgrade schema file in some cases (as suggested by chealer and jonny, it might be a workaround but at least this fixes the issue finally (tested), thanks; fixes https://dev.tiki.org/item6621 and https://dev.tiki.org/item6561)

--- installer/schema/20171123_create_object_links_tiki.php
+++ installer/schema/20171123_create_object_links_tiki.php
@@ -12,6 +12,7 @@ if (strpos($_SERVER["SCRIPT_NAME"], basename(__FILE__)) !== false) {
  */
 function upgrade_20171123_create_object_links_tiki($installer)
 {
+	include_once('tiki-setup.php');
 
 	$create_links = function ($installer, $type, $objectId, $data) {
 		$parserlib = TikiLib::lib('parser');
