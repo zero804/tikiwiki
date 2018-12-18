bfa6c56e7333d91d6e9c822ba827a8ec0ab96780
robertokir <robertokir@users.sourceforge.net>
2018-12-18 20:01:15 +0000

    [bp/68155][REF] Refactor fix runtime exception caused by missing file required by blueimp/jquery-file-upload classmap (thanks Xorti)

---
+++ lib/core/Tiki/Composer/Autoload.php
@@ -0,0 +1,39 @@
+<?php
+
+// $Id$
+
+namespace Tiki\Composer;
+
+use Composer\Script\Event;
+
+class Autoload
+{
+	public static function change(Event $event)
+	{
+		$composer = $event->getComposer();
+		$vendors = $composer->getConfig()->get('vendor-dir');
+
+		if (substr($vendors, -1, 1) !== DIRECTORY_SEPARATOR) {
+			$vendors .= DIRECTORY_SEPARATOR;
+		}
+
+		$repoManager = $composer->getRepositoryManager()->getLocalRepository();
+		$package = $repoManager->findPackages('blueimp/jquery-file-upload');
+
+		if (! empty($package[0])) {
+			$autoload = $package[0]->getAutoload();
+			if (! empty($autoload['classmap'])) {
+				foreach ($autoload['classmap'] as $key => $path) {
+					if ($path == 'server/php/UploadHandler.php') {
+						unset($autoload['classmap'][$key]);
+						break;
+					}
+				}
+				$package[0]->setAutoload($autoload);
+			}
+		}
+
+		// Write changes to local repository file vendor_bundled/vendor/composer/installed.json
+		$repoManager->write();
+	}
+}
