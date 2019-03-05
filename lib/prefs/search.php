5dd86743e0d87d01f529efca0cd09b5fb4d484b4
jonnybradley <jonnybradley@users.sourceforge.net>
2019-03-05 04:24:52 +0000

    [bp/r69358][ENH] search: Add a new pref to allow some facets (aggregations) to not be displayed on the default search page tiki-searchindex.php

--- lib/prefs/search.php
+++ lib/prefs/search.php
@@ -90,6 +90,9 @@ function prefs_search_list()
 			'filter' => 'digits',
 			'units' => tra('facet results'),
 			'default' => '10',
+			'dependencies' => [
+				'search_use_facets',
+			],
 		],
 		'search_index_outdated' => [
 			'name' => tra('Search index outdated'),
@@ -113,6 +116,18 @@ function prefs_search_list()
 			'type' => 'flag',
 			'packages_required' => ['media-alchemyst/media-alchemyst' => 'MediaAlchemyst\Alchemyst'],
 			'default' => 'n',
+		],
+		'search_excluded_facets' => [
+			'name' => tra('Excluded facets'),
+			'description' => tra('List of facets (a.k.a. aggregations) to exclude from the default search results'),
+			'hint' => 'For example: object_type,title_initial,title_firstword,tracker_field_userName',
+			'type' => 'text',
+			'filter' => 'word',
+			'separator' => ',',
+			'default' => [],
+			'dependencies' => [
+				'search_use_facets',
+			],
 		],
 	];
 }
