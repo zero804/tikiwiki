f0deef182f9a4ef4dd15cd241217770f77e59597
rjsmelo <rjsmelo@users.sourceforge.net>
2019-03-16 21:24:47 +0000

    [bp/r69464][FIX] Plugin Diagrams not working correctly in IE11

--- tiki-editdiagram.php
+++ tiki-editdiagram.php
@@ -145,8 +145,8 @@ $js = "(function()
 
 			// Main
 			var ui = new EditorUi(new Editor(urlParams['chrome'] == '0', themes));
-			var xml = `{$xmlDiagram}`;
-			handleXmlData(ui.editor.graph, `{$xmlDiagram}`);
+			var xml = '{$xmlDiagram}';
+			handleXmlData(ui.editor.graph, '{$xmlDiagram}');
 
 		}, function()
 		{
