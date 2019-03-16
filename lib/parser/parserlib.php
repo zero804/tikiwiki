257ea58e0954b6d6678460ea4de2186abebddb6b
rjsmelo <rjsmelo@users.sourceforge.net>
2019-03-16 23:35:49 +0000

    [bp/r69467][ENH] Allow to define the size of the preview for files in the search

--- lib/parser/parserlib.php
+++ lib/parser/parserlib.php
@@ -3389,6 +3389,8 @@ class ParserLib extends TikiDb_Bridge
 			$userLib = TikiLib::lib('user');
 			$guesser = new Guesser();
 			MimeTypeGuesser::getInstance()->register($guesser);
+			$maxWidthPreview = $prefs['fgal_maximum_image_width_preview'];
+
 			foreach ($fileIds as $fileId) {
 				$info = $fileGalleryLib->get_file($fileId);
 				if (empty($info)) {
@@ -3399,7 +3401,13 @@ class ParserLib extends TikiDb_Bridge
 				}
 				$search = 'data-type="file" data-object="' . $fileId . '"';
 				if (strpos($info['filetype'], 'image') !== false) {
-					$popup = 'data-type="file" data-object="' . $fileId . '" data-toggle="popover" data-trigger="hover focus" data-content="<img src=\'tiki-download_file.php?fileId=' . $fileId . '&amp;thumbnail\'>" data-html="1"';
+					$appendMaxSize = '';
+
+					if (! empty($maxWidthPreview)) {
+						$appendMaxSize = "&amp;x=" . $maxWidthPreview;
+					}
+
+					$popup = 'data-type="file" data-object="' . $fileId . '" data-toggle="popover" data-trigger="hover focus" data-content="<img src=\'tiki-download_file.php?fileId=' . $fileId . '&amp;thumbnail' . $appendMaxSize . '\'>" data-html="1" data-width="'. $maxWidthPreview . '"';
 					$content = str_replace($search, $popup, $content);
 				} else {
 					$filePath = '';
@@ -3449,7 +3457,17 @@ class ParserLib extends TikiDb_Bridge
 						$guesser->add($sourceFile, $info['filetype']);
 
 						$alchemy = new Tiki\Lib\Alchemy\AlchemyLib();
-						$alchemy->convertToImage($sourceFile, $targetFile, '200px', '400px', false);
+
+						$height = 400;
+						$width = 200;
+						if (! empty($prefs['fgal_maximum_image_width_preview'])) {
+							$originalHeight = $height;
+							$originalWidth = $width;
+							$width = $prefs['fgal_maximum_image_width_preview'];
+							$height = (int) $originalHeight * $width / $originalWidth;
+						}
+
+						$alchemy->convertToImage($filePath, $targetFile, $height, $width, false);
 
 						// Restore the environment
 						if ($envHomeDefined) {
@@ -3468,7 +3486,7 @@ class ParserLib extends TikiDb_Bridge
 					}
 
 					if ($cacheLib->isCached($cacheName, $cacheType)) {
-						$popup = 'data-type="file" data-object="' . $fileId . '" data-toggle="popover" data-trigger="hover focus" data-content="<img src=\'tiki-download_file.php?fileId=' . $fileId . '&amp;preview\'>" data-html="1"';
+						$popup = 'data-type="file" data-object="' . $fileId . '" data-toggle="popover" data-trigger="hover focus" data-content="<img src=\'tiki-download_file.php?fileId=' . $fileId . '&amp;preview\'>" data-html="1" data-width="' . $maxWidthPreview . '"';
 						$content = str_replace($search, $popup, $content);
 					}
 				}
