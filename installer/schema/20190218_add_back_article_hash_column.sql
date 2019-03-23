aa5e7623960f8f727de8ca5ee79128b1ae1dcaa2
rjsmelo <rjsmelo@users.sourceforge.net>
2019-03-23 16:55:32 +0000

    [FIX]In r69140 the file was added without "_tiki", this allows the script to be run also when the script without _tiki was run already

--- installer/schema/20190218_add_back_article_hash_column.sql
+++
@@ -1,1 +0,0 @@
-ALTER TABLE `tiki_articles` ADD COLUMN `hash` VARCHAR(32) DEFAULT '' AFTER `body`;
---
+++ installer/schema/20190218_add_back_article_hash_column_tiki.php
@@ -0,0 +1,23 @@
+<?php
+// (c) Copyright by authors of the Tiki Wiki CMS Groupware Project
+//
+// All Rights Reserved. See copyright.txt for details and a complete list of authors.
+// Licensed under the GNU LESSER GENERAL PUBLIC LICENSE. See license.txt for details.
+// $Id$
+
+if (strpos($_SERVER["SCRIPT_NAME"], basename(__FILE__)) !== false) {
+       header("location: index.php");
+       exit;
+}
+
+/**
+ * In r69140 the file was added without "_tiki", this allows the script to be run also when the script without _tiki was run already
+ *
+ * @param Installer $installer
+ */
+function upgrade_20190218_add_back_article_hash_column_tiki($installer)
+{
+       if (empty($installer->query("SHOW COLUMNS FROM `tiki_articles` LIKE 'hash';")->result)) {
+               $installer->query("ALTER TABLE `tiki_articles` ADD COLUMN `hash` VARCHAR(32) DEFAULT NULL AFTER `body`;");
+       }
+}
