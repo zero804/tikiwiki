681dca24fde3c8c838b89c8ec1554aa63d190029
jonnybradley <jonnybradley@users.sourceforge.net>
2019-06-11 17:01:09 +0000

    [bp/r70198][FIX] trackers: If using a field as |output and the user only has canViewField perm on it we need the trackerId after the form has been posted to do renderOutput() on it.

--- lib/wiki-plugins/wikiplugin_tracker.php
+++ lib/wiki-plugins/wikiplugin_tracker.php
@@ -1049,6 +1049,7 @@ function wikiplugin_tracker($data, $params)
 			} else if ($itemObject->canViewField($f['fieldId']) && ! empty($item_info)) {
 				$flds['data'][] = [
 					'fieldId' =>$f['fieldId'],
+					'trackerId' =>$f['trackerId'],
 					'permName' =>$f['permName'],
 					'type' =>$f['type'],
 					'readonly' => true,
