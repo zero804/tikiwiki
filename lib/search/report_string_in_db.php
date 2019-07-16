44079c7362539a1dbb145da7f2e113de7133ad47
robertokir <robertokir@users.sourceforge.net>
2019-07-16 13:28:27 +0000

    [bp/r69384][FIX] Report all occurrences of a string in any table: Link now uses SEFURL and deals with spaces in page name; formatted some datetime columns (thanks Jonny) [bp/r69388][FIX] Report all occurrences of a string in any table: if you search for something with a double quote in it, when you go to the second level of search per table it truncated at the quote char (thanks Jonny) [bp/r69547][ENH] Admin Search tool/Occurrences of string in database: in second level search on table tiki_pages/column data, show a column with a snippet of text found and search string highlighted; when hovered over, show a popup with wiki-parsed page content (thanks Jonny and Luci) [bp/r69570][ENH] Admin Search tool/Occurrences of string in database: Search button works with "enter" key (thanks Jonny and Chealer); 2nd level results in responsive table; added button to go back to 1st level results. [bp/r69697][ENH] Admin Search tool/Occurrences of string in database: Pulldown menu to pick a table at the beginning of the search [bp/r69827][FIX] sefurl: In some setups this tries to redeclare smarty_modifier_sefurl as smarty_modifier_sefUrl which causes an error. Also the 'wiki' type parameter isn't required as that is the default (and should be a string any way). [bp/r70149][FIX] fix double class typos in search report template

--- lib/search/report_string_in_db.php
+++ lib/search/report_string_in_db.php
@@ -41,6 +41,22 @@ if (! empty($_POST['string_in_db_search'])) {
 	$sql = "select * from `" . $table . "` where `" . $column . "` like ?";
 	$rs = $tikilib->fetchAll($sql, $args);
 	foreach ($rs as $row) {
+		if ($table == 'tiki_pages') {
+			$stringpos = strpos($row['data'], $_POST['query']);
+			$stringend = $stringpos + strlen($_POST['query']);
+			$startsnip = max($stringpos - 100, 0);
+			$endsnip = $stringend + 100;
+			$length = ($endsnip - $startsnip);
+			$snippet = substr($row['data'], $startsnip, $length);
+			$snippet = str_replace($_POST['query'],"<span class='highlight'>".$_POST['query']."</span>", $snippet);
+			if ($startsnip > 0) {
+				$snippet = '...' . $snippet;
+			}
+			if ($endsnip < strlen($row['data'])) {
+				$snippet = $snippet . '...';
+			}
+			$row['snippet'] = $snippet;
+		}
 		$tableData[] = $row;
 	}
 	$smarty->assign('tableData', $tableData);
@@ -63,7 +79,7 @@ function searchAllDB($search)
 	foreach ($rs as $key => $val) {
 		$vals = array_values($val);
 		$table = $vals[0];
-		if (substr($table, 0, 6) == 'index_') {
+		if (substr($table, 0, 6) == 'index_' && substr($table,0, 10) !== 'index_pref') {
 			continue;
 		}
 		$sql2 = "SHOW COLUMNS FROM `$table`";
