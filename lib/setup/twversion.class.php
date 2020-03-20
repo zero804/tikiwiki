620d9d96c17cfc4b17f22ebea0e9dfe48a1bc487
Roberto Kirschbaum <robertokir.pro@gmail.com>
2020-03-20 15:00:36 +0000

    [REL] Closing 20.2 release

--- lib/setup/twversion.class.php
+++ lib/setup/twversion.class.php
@@ -27,7 +27,7 @@ class TWVersion
 		$this->branch 	= 'stable';
 
 		// Set everything else, including defaults.
-		$this->version 	= '20.2';	// needs to have no spaces for releases
+		$this->version 	= '20.3svn';	// needs to have no spaces for releases
 		$this->star		= 'Tarazed';
 		$this->releases	= [];
 
