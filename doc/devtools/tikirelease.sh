95ec5d100bf2d5e4749e3654597124c9f2565b01
Roberto Kirschbaum <robertokir.pro@gmail.com>
2020-06-13 11:14:13 +0000

    [REL] Release: workaround for composer 2.0

--- doc/devtools/tikirelease.sh
+++ doc/devtools/tikirelease.sh
@@ -55,7 +55,8 @@ echo "Exporting $SVNROOT/$RELTAG $MODULE-$VER"
 svn export $SVNROOT/$RELTAG $MODULE-$VER
 
 if [ -f $MODULE-$VER/composer.json ]; then
-	wget -N http://getcomposer.org/composer.phar
+  #temporary fix due to composer 2.0 alpha, use composer.phar from somewhere else
+	#wget -N http://getcomposer.org/composer.phar
 	cd $MODULE-$VER
 	php ../composer.phar install --prefer-dist --no-dev 2>&1 | sed '/Warning: Ambiguous class resolution/d'
 	cd ..
