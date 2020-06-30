9b03ff78f1d1edc982f6809cacd1bd5a9882275a
Jonny Bradley <jmail@nospaces.net>
2020-06-30 16:39:38 +0100

    Revert "[FIX] ajax: Check $_GET for controller and action for filtering as well, and add request vars used by templates to the config"
    This reverts commit 762dc014 (sorry, wrong branch)

--- tiki-ajax_services.php
+++ tiki-ajax_services.php
@@ -21,12 +21,10 @@ $inputConfiguration = [[
 	'staticKeyFilters' => [
 		'action' => 'word',
 		'controller' => 'text',
-		'sort_mode' => 'word',
-		'watch' => 'alpha',
 	],
 ]];
 
-if (isset($_REQUEST['controller'], $_REQUEST['action']) || isset($_GET['controller'], $_GET['action'])) {
+if (isset($_REQUEST['controller'], $_REQUEST['action'])) {
 	$inputConfiguration[] = ['catchAllUnset' => null];
 }
 
