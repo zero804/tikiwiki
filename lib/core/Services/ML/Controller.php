dc0e88d8b0150fb34caf651c1ab641aa4dff02e6
Victor Emanouilov <kroky6@gmail.com>
2020-09-18 14:01:55 +0300

    [NEW] ML: ability to define machine learning models and interactively add RubixML transformers and learners with proper parameters (WIP)

---
+++ lib/core/Services/ML/Controller.php
@@ -0,0 +1,329 @@
+<?php
+// (c) Copyright by authors of the Tiki Wiki CMS Groupware Project
+//
+// All Rights Reserved. See copyright.txt for details and a complete list of authors.
+// Licensed under the GNU LESSER GENERAL PUBLIC LICENSE. See license.txt for details.
+// $Id$
+
+//this script may only be included - so its better to die if called directly.
+if (strpos($_SERVER['SCRIPT_NAME'], basename(__FILE__)) !== false) {
+	header('location: index.php');
+	exit;
+}
+
+/**
+ * Class Services_ML_Controller
+ */
+class Services_ML_Controller
+{
+	private $mllib;
+
+	const LEARNERS = [
+		'Classifiers' => [
+			'path' => 'Classifiers',
+			'classes' => [
+				'AdaBoost',
+				'ClassificationTree',
+				'ExtraTreeClassifier',
+				'GaussianNB',
+				'KDNeighbors',
+				'KNearestNeighbors',
+				'LogisticRegression',
+				'MultilayerPerceptron',
+				'NaiveBayes',
+				'RadiusNeighbors',
+				'RandomForest',
+				'SoftmaxClassifier',
+				'SVC',
+			]
+		],
+		'Regressors' => [
+			'path' => 'Regressors',
+			'classes' => [
+				'Adaline',
+				'ExtraTreeRegressor',
+				'GradientBoost',
+				'KDNeighborsRegressor',
+				'KNNRegressor',
+				'MLPRegressor',
+				'RadiusNeighbors',
+				'RegressionTree',
+				'Ridge',
+				'SVR',
+			]
+		],
+		'Clusterers' => [
+			'path' => 'Clusterers',
+			'classes' => [
+				'DBSCAN',
+				'FuzzyCMeans',
+				'GaussianMixture',
+				'KMeans',
+				'MeanShift',
+			]
+		],
+		'Anomaly Detectors' => [
+			'path' => 'AnomalyDetectors',
+			'classes' => [
+				'GaussianMLE',
+				'IsolationForest',
+				'LocalOutlierFactor',
+				'Loda',
+				'OneClassSVM',
+				'RobustZScore',
+			]
+		],
+		'Transformers: Dimensionality Reduction' => [
+			'path' => 'Transformers',
+			'classes' => [
+				'DenseRandomProjector',
+				'GaussianRandomProjector',
+				'LinearDiscriminantAnalysis',
+				'PrincipalComponentAnalysis',
+				'SparseRandomProjector',
+			]
+		],
+		'Transformers: Feature Conversion' => [
+			'path' => 'Transformers',
+			'classes' => [
+				'IntervalDiscretizer',
+				'OneHotEncoder',
+				'NumericStringConverter',
+			]
+		],
+		'Transformers: Feature Selection' => [
+			'path' => 'Transformers',
+			'classes' => [
+				'RecursiveFeatureEliminator',
+				'VarianceThresholdFilter',
+			]
+		],
+		'Transformers: Image Transformers' => [
+			'path' => 'Transformers',
+			'classes' => [
+				'ImageResizer',
+				'ImageVectorizer',
+			]
+		],
+		'Transformers: Imputation' => [
+			'path' => 'Transformers',
+			'classes' => [
+				'KNNImputer',
+				'MissingDataImputer',
+				'RandomHotDeckImputer',
+			]
+		],
+		'Transformers: Other' => [
+			'path' => 'Transformers',
+			'classes' => [
+				'PolynomialExpander',
+			]
+		],
+		'Transformers: Standardization and Normalization' => [
+			'path' => 'Transformers',
+			'classes' => [
+				'L1Normalizer',
+				'L2Normalizer',
+				'MaxAbsoluteScaler',
+				'MinMaxNormalizer',
+				'RobustStandardizer',
+				'ZScaleStandardizer',
+			]
+		],
+		'Transformers: Text' => [
+			'path' => 'Transformers',
+			'classes' => [
+				'HTMLStripper',
+				'RegexFilter',
+				'TextNormalizer',
+				'MultibyteTextNormalizer',
+				'StopWordFilter',
+				'BM25Transformer',
+				'TfIdfTransformer',
+				'DeltaTfIdfTransformer',
+				'WhitespaceTrimmer',
+				'WordCountVectorizer',
+			]
+		],
+	];
+
+	const TOKENIZERS = [
+		'path' => 'Other\Tokenizers',
+		'classes' => [
+			'NGram',
+			'SkipGram',
+			'Whitespace',
+			'Word',
+			'WordStemmer',
+		]
+	];
+
+	public function setUp()
+	{
+		$this->mllib = TikiLib::lib('ml');
+		Services_Exception_Disabled::check('feature_machine_learning');
+
+		$perms = Perms::get();
+		if (! $perms->machine_learning && ! $perms->admin) {
+			throw new Services_Exception_Denied;
+		}
+	}
+
+	public function action_list()
+	{
+		$models = $this->mllib->get_models();
+		return [
+			'title' => tr('Machine Learning Models'),
+			'models' => $models
+		];
+	}
+
+	public function action_create($input)
+	{
+		Services_Exception_Denied::checkGlobal('tiki_p_admin_machine_learning');
+
+		if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+			$mlmId = $this->mllib->set_model(null, $this->serializeInput($input));
+
+			$forward = [
+				'controller' => 'ml',
+				'action' => 'edit',
+				'tabularId' => $mlmId
+			];
+			return ['FORWARD' => $forward];
+		}
+
+		return [
+			'title' => tr('Create Machine Learning Model'),
+		];
+	}
+
+	public function action_edit($input)
+	{
+		Services_Exception_Denied::checkGlobal('tiki_p_admin_machine_learning');
+
+		$model = $this->mllib->get_model($input->mlmId->int());
+		if ($definition = Tracker_Definition::get($model['sourceTrackerId'])) {
+			$fields = $definition->getFields();
+		} else {
+			$fields = [];
+		}
+
+		if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+			$mlmId = $this->mllib->set_model($model['mlmId'], $this->serializeInput($input));
+
+			Feedback::success('Model was updated successfully. You might want to train against the source dataset.');
+
+			$forward = [
+				'controller' => 'ml',
+				'action' => 'list',
+			];
+			return ['FORWARD' => $forward];
+		}
+
+		return [
+			'title' => tr('Edit Machine Learning Model'),
+			'model' => $model,
+			'fields' => $fields,
+			'learners' => self::LEARNERS
+		];
+	}
+
+	public function action_model_args($input)
+	{
+		$class = $input->class->text();
+		if (empty($class)) {
+			throw new Services_Exception_NotFound(tr('No class chosen.'));
+		}
+
+		$args = [];
+		try {
+			$ref = new ReflectionClass('Rubix\ML\\'.$class);
+			$constructor = $ref->getConstructor();
+			if ($constructor) {
+				foreach ($constructor->getParameters() as $key => $param) {
+					$type = $param->getType();
+					if ($type->isBuiltin()) {
+						$input_type = 'text';
+					} else {
+						$input_type = $type->getName();
+					}
+					$args[] = [
+						'name' => $param->getName(),
+						'default' => $param->getDefaultValue(),
+						'arg_type' => $type->getName(),
+						'input_type' => $input_type,
+					];
+				}
+			}
+		} catch (ReflectionException $e) {
+			throw new Services_Exception_Denied($e->getMessage());
+		}
+
+		if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+			$arg_values = $input->args->array();
+			foreach ($args as $key => $arg) {
+				if (isset($arg_values[$arg['name']]) && !empty($arg_values[$arg['name']])) {
+					if ($arg['arg_type'] == 'array') {
+						$args[$key]['value'] =  explode(',', $arg_values[$arg['name']]);
+					} else {
+						$args[$key]['value'] = $arg_values[$arg['name']];
+					}
+				} else {
+					$args[$key]['value'] =  $arg['default'];
+				}
+			}
+			$payload = [[
+				'class' => $class,
+				'args' => $args,
+			]];
+			$instances = $this->mllib->hydrate(json_encode($payload));
+			return [
+				'learner' => preg_replace('/^[^\\\\]*\\\\/', '', $instances[0]['class']),
+				'arguments' => (string)$instances[0]['instance'],
+				'payload' => $payload[0],
+			];
+		}
+
+		return [
+			'title' => tr('%0 arguments', preg_replace('/^[^\\\\]*\\\\/', '', $class)),
+			'class' => $class,
+			'args' => $args,
+			'tokenizers' => self::TOKENIZERS
+		];
+	}
+
+	public function action_delete($input)
+	{
+		$mlmId = $input->mlmId->int();
+
+		Services_Exception_Denied::checkGlobal('tiki_p_admin_machine_learning');
+
+		if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+			$this->mllib->delete_model($mlmId);
+		}
+
+		return [
+			'title' => tr('Remove Model'),
+			'mlmId' => $mlmId,
+		];
+	}
+
+	protected function serializeInput($input)
+	{
+		$trackerId = $input->trackerId->int();
+		$definition = Tracker_Definition::get($trackerId);
+
+		if( !$definition ) {
+			throw new Services_Exception_NotFound(tr('Tracker %0 not found', $trackerId));
+		}
+
+		return [
+			'name' => $input->name->text(),
+			'description' => $input->description->text(),
+			'sourceTrackerId' => $trackerId,
+			'trackerFields' => $input->fields->array(),
+			'payload' => $input->payload->text(),
+		];
+	}
+}
---
+++ templates/ml/edit.tpl
@@ -0,0 +1,127 @@
+{extends "layout_view.tpl"}
+
+{block name="title"}
+	{title}{$title}{/title}
+{/block}
+
+{block name="navigation"}
+	<div class="t_navbar mb-4">
+		<a class="btn btn-link" href="{service controller=ml action=create}">{icon name=create} {tr}New{/tr}</a>
+		<a class="btn btn-link" href="{service controller=ml action=list}">{icon name=list} {tr}Manage{/tr}</a>
+	</div>
+{/block}
+
+{block name="content"}
+	<div class="table-responsive">
+		<form class="edit-ml" method="post" action="{service controller=ml action=edit mlmId=$model.mlmId}">
+			<input type="hidden" name="trackerId" value="{$model.sourceTrackerId}">
+			<div class="form-group row">
+				<label class="col-form-label col-sm-2">{tr}Name{/tr}</label>
+				<div class="col-sm-10">
+					<input class="form-control" type="text" name="name" value="{$model.name|escape}" required>
+				</div>
+			</div>
+			<div class="form-group row">
+				<label class="col-form-label col-sm-2">{tr}Description{/tr}</label>
+				<div class="col-sm-10">
+					<textarea class="form-control" name="description">{$model.description|escape}</textarea>
+				</div>
+			</div>
+			<div class="form-group row">
+				<label class="col-form-label col-sm-2">{tr}Source tracker{/tr}</label>
+				<div class="col-sm-10">
+					{object_link type=tracker id=$model.sourceTrackerId}
+				</div>
+			</div>
+			<div class="form-group row">
+				<label class="col-form-label col-sm-2">{tr}Dimension fields{/tr}</label>
+				<div class="col-sm-10">
+					<select class="form-control" name="fields[]" multiple size="10">
+						<option value="">Item title</option>
+						{foreach $fields as $field}
+							<option value="{$field.fieldId|escape}" {if in_array($field.fieldId, $model.trackerFields)}selected{/if}>{$field.name|escape}</option>
+						{/foreach}
+					</select>
+				</div>
+			</div>
+			<div class="form-group row">
+				<label class="col-form-label col-sm-2">{tr}ML Model{/tr}</label>
+				<div class="col-sm-10">
+					<table class="table model">
+						<thead>
+							<tr>
+								<th>
+									{tr}Transformers and Learner{/tr}
+									<a class="tikihelp alert-link" title="|RubixML Help" target="tikihelp" href="https://docs.rubixml.com/en/latest/choosing-an-estimator.html">
+										{icon name="help"}
+									</a>
+								</th>
+								<th>{tr}Arguments{/tr}</th>
+								<th></th>
+							</tr>
+						</thead>
+						<tbody>
+							<tr class="d-none">
+								<td>
+									<div class="input-group input-group-sm">
+										<div class="input-group-prepend">
+											<span class="input-group-text">{icon name=sort}</span>
+										</div>
+										<input class="learner form-control" disabled>
+									</div>
+								</td>
+								<td>
+									<a href="#" class="arguments"></a>
+									<textarea class="serialized-args d-none"></textarea>
+								</td>
+								<td class="text-right"><button class="remove btn-sm btn-outline-warning">{icon name=remove}</button></td>
+							</tr>
+							{foreach $model.instances as $instance}
+								<tr>
+									<td>
+										<div class="input-group input-group-sm">
+											<div class="input-group-prepend">
+												<span class="input-group-text">{icon name=sort}</span>
+											</div>
+											<input class="learner form-control" disabled value="{$instance.learner|escape}">
+										</div>
+									</td>
+									<td>
+										<a href="{service controller=ml action=model_args mlmId=$model.mlmId class=$instance.class}" class="arguments">{$instance.instance|escape}</a>
+										<textarea class="serialized-args d-none">{$instance.serialized_args}</textarea>
+									</td>
+									<td class="text-right"><button class="remove btn-sm btn-outline-warning">{icon name=remove}</button></td>
+								</tr>
+							{/foreach}
+						</tbody>
+						<tfoot>
+							<tr>
+								<td>
+									<select class="selection form-control">
+										<option disabled="disabled" selected="selected">{tr}Select...{/tr}</option>
+										{foreach $learners as $label => $group}
+											<optgroup label="{$label|escape}">
+												{foreach $group.classes as $class}
+													<option value="{$group.path|escape}\{$class|escape}">{$class|escape}</option>
+												{/foreach}
+											</optgroup>
+										{/foreach}
+									</select>
+								</td>
+								<td>
+									<a href="{service controller=ml action=model_args mlmId=$model.mlmId}" class="btn btn-secondary add-learner">{tr}Enter Arguments{/tr}</a>
+									<textarea name="payload" class="d-none">{$model.payload}</textarea>
+								</td>
+							</tr>
+						</tfoot>
+					</table>
+				</div>
+			</div>
+			<div class="form-group row submit">
+				<div class="col-sm-10 offset-sm-2">
+					<input type="submit" class="btn btn-primary" value="{tr}Update{/tr}">
+				</div>
+			</div>
+		</form>
+	</div>
+{/block}
