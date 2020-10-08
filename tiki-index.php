zero804/tikiwiki

commit 936f82d711594b601ad75f08206896b4746741ca

[FIX] wiki: Get real page name from slug before trying to use it

tiki-index.php
@@ -90,6 +90,11 @@ if (! isset($_SESSION['thedate'])) {
 $perspectivelib = TikiLib::lib('perspective');
 $activeWS = $perspectivelib->get_current_perspective(null);
 
+$originalPageRequested = $page = $_REQUEST['page'];
+if ($prefs['wiki_url_scheme'] !== 'urlencode') {
+	$page = $_REQUEST['page'] = TikiLib::lib('wiki')->get_page_by_slug($page);
+}
+
 // If there's a WS active and the WS has a homepage, then load the WS homepage
 if ((! empty($activeWS)) and $isHomePage) {
 	$preferences = $perspectivelib->get_preferences($activeWS);
@@ -178,10 +183,6 @@ if (! empty($page_ref_id)) {
 	$smarty->assign('page_ref_id', $page_ref_id);
 }
 
-$originalPageRequested = $page = $_REQUEST['page'];
-if ($prefs['wiki_url_scheme'] !== 'urlencode') {
-	$page = $_REQUEST['page'] = TikiLib::lib('wiki')->get_page_by_slug($page);
-}
 $smarty->assign_by_ref('page', $page);
 
 $cat_type = 'wiki page';

commit ad8a39044f749e4f1ffebba9846bfa2e63ac4c00

[FIX] structures: Fix page loading when using page_ref_id on the url instead of the page name (presumably to avoid all that wiki-goodness ;) Works on pages in structures only (regression from 936f82d7 - my bad, apologies)

tiki-index.php
@@ -176,7 +176,7 @@ if (! empty($page_ref_id)) {
 	$info = null;
 	// others still need a good set page name or they will get confused.
 	// comments of home page were all visible on every structure page
-	$_REQUEST['page'] = $page_info['pageName'];
+	$page = $_REQUEST['page'] = $page_info['pageName'];
 } else {
 	$page_ref_id = '';
 	$smarty->assign('showstructs', $structs_with_perm);