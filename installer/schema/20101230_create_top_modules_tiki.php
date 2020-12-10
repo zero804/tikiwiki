c2c53b8de23889ed797f6911ad1594c079103920
Jorge Sá Pereira <me@jorgesapereira.com>
2020-12-10 16:39:19 +0000

    [FIX] Fix issue with groups being a reserved word in MySQL8
    (cherry picked from commit 61081ee53c30fd6ec770d102fd8254258030124f)

--- installer/schema/20101230_create_top_modules_tiki.php
+++ installer/schema/20101230_create_top_modules_tiki.php
@@ -50,14 +50,14 @@ function upgrade_20101230_create_top_modules_tiki($installer)
 	// add site logo
 	if ($prefs['feature_sitelogo'] === 'y') {
 		$installer->query(
-			"INSERT INTO `tiki_modules` (name,position,ord,cache_time,params,groups) VALUES " .
+			"INSERT INTO `tiki_modules` (name,position,ord,cache_time,params,`groups`) VALUES " .
 			"('logo','t',1,7200,'nobox=y','a:0:{}');"
 		);
 	}
 	// add site login
 	if ($prefs['feature_site_login'] === 'y') {
 		$installer->query(
-			"INSERT INTO `tiki_modules` (name,position,ord,cache_time,params,groups) VALUES " .
+			"INSERT INTO `tiki_modules` (name,position,ord,cache_time,params,`groups`) VALUES " .
 			"('login_box','t',2,0,'mode=popup&nobox=y','a:0:{}');"
 		);
 	}
@@ -67,14 +67,14 @@ function upgrade_20101230_create_top_modules_tiki($installer)
 		if ($prefs['feature_sitemenu'] === 'y') {
 			$menuId = $prefs['feature_topbar_id_menu'];
 			$installer->query(
-				"INSERT INTO `tiki_modules` (name,position,ord,cache_time,params,groups) VALUES " .
+				"INSERT INTO `tiki_modules` (name,position,ord,cache_time,params,`groups`) VALUES " .
 				"('menu','o',2,7200,'id=$menuId&type=horiz&menu_id=tiki-top&menu_class=clearfix&nobox=y','a:0:{}');"
 			);
 		}
 		// add site search
 		if ($prefs['feature_sitesearch'] === 'y') {
 			$installer->query(
-				"INSERT INTO `tiki_modules` (name,position,ord,cache_time,params,groups) VALUES " .
+				"INSERT INTO `tiki_modules` (name,position,ord,cache_time,params,`groups`) VALUES " .
 				"('search','o',1,7200,'nobox=y','a:0:{}');"
 			);
 		}
@@ -84,7 +84,7 @@ function upgrade_20101230_create_top_modules_tiki($installer)
 		$sitemycode = $installer->getOne("SELECT `value` FROM `tiki_preferences` WHERE `name` = 'sitemycode'");
 		if (strpos($sitemycode, 'quickadmin') !== false) {
 			$installer->query(
-				"INSERT INTO `tiki_modules` (name,position,ord,cache_time,params,groups) VALUES" .
+				"INSERT INTO `tiki_modules` (name,position,ord,cache_time,params,`groups`) VALUES" .
 				" ('quickadmin','t',3,7200,'nobox=y','a:1:{i:0;s:6:\"Admins\";}');"
 			);
 		}
@@ -92,7 +92,7 @@ function upgrade_20101230_create_top_modules_tiki($installer)
 	// add breadcrumb module - feature_breadcrumbs stays for now
 	if ($prefs['feature_breadcrumbs'] === 'y') {
 		$installer->query(
-			"INSERT INTO `tiki_modules` (name,position,ord,cache_time,params,groups) VALUES " .
+			"INSERT INTO `tiki_modules` (name,position,ord,cache_time,params,`groups`) VALUES " .
 			"('breadcrumbs','t',6,0,'nobox=y','a:0:{}');"
 		);
 	}
